48047c1bf49925ef9e2e2caa4532ec74d35fb3ef
unknown <jfar80@gmail.com>
2023-01-25 12:23:57 -0500

    funcionando a 130

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,5 @@
 const { response, request} = require('express');
-const {validationResult} = require('express-validator');
+
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
@@ -21,13 +21,6 @@ const usuariosPut = (req, res=response)=> {
    });
 }
 const usuariosPost = async (req, res=response)=> {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json(errors);
-    }
-
-
 
     const {nombre, correo, password, rol} =req.body;
     const usuario = new Usuario({nombre, correo, password,rol});
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,7 @@
 const {Router}=require('express');
-const {check}=require('express-validator')
+const {check}=require('express-validator');
+const Role = require('../models/role');
+const {validarCampos} = require('../middlewares/validar-campos');
 const { usuariosGet, usuariosPatch, usuariosPut, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
 
 const router = Router();
@@ -9,7 +11,17 @@ router.get('/',  usuariosGet);
 router.put('/:id',  usuariosPut);
 
 router.post('/', [
-    check('correo', 'El correo no es valido').isEmail(),
+    check('nombre', 'El nombre obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser de mas de 6 letras').isLength({min:6}),
+    check('correo', 'El password debe ser de mas de 6 letras').isLength({min:6}),
+    //check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(async(rol='') => {
+        const existeRol = await Role.findOne({ rol });
+        if (!existeRol){
+            throw new Error(`El rol ${rol} no esta registrado en la base de datos`)
+        }
+    }),
+    validarCampos   
 ], usuariosPost);
 
 router.patch('/',  usuariosPatch);

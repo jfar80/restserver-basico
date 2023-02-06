dee4fae93e5ac12139775242f70b491cc1c8c3fa
unknown <jfar80@gmail.com>
2023-02-06 17:03:11 -0500

    Fin seccion 13 - version 6.0.0

--- models/server.js
+++ models/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
+
 
 
 class Server{
@@ -16,6 +18,7 @@ class Server{
         usuarios: '/api/usuarios',
         categorias:'/api/categorias',
         productos:'/api/productos',
+        uploads:'/api/uploads'   
      }
      
 
@@ -42,6 +45,13 @@ class Server{
 
         //Directorio publico
         this.app.use(express.static('public'));
+
+        //Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes(){
@@ -50,6 +60,7 @@ class Server{
         this.app.use(this.paths.usuarios,require('../routes/usuarios'));
         this.app.use(this.paths.categorias,require('../routes/categorias'));
         this.app.use(this.paths.productos,require('../routes/productos'));
+        this.app.use(this.paths.uploads,require('../routes/uploads'));
        
         
 }
